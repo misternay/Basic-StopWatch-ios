0ba2dc96845da76d6f2ad413b5ebced7545af9b9
MisterNay <misternay@misternays-macbook-pro.local>
2017-07-18 14:33:33 +0700

    Add function stopwatch

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,9 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var counter = 0.0
+    
+    var timer = Timer()
+    
+    var isPlaying = false
+    
+    @IBAction func startButton(_ sender: AnyObject) {
+        if(isPlaying) {
+            return
+        }
+        startButton.isEnabled = false
+        pauseButton.isEnabled = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        isPlaying = true
+    }
+    
+    @IBAction func pauseButton(_ sender: AnyObject) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+    }
+    
+    @IBAction func resetButton(_ sender: AnyObject) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+        timeLabel.text = String(counter)
+    }
+    func UpdateTimer() {
+        counter = counter + 0.1
+        timeLabel.text = String(format: "%.1f", counter)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timeLabel.text = String(counter)
+        
+        pauseButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
